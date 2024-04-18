97669848460042ff873d69644cc0a36eb8383a9a
Yik Yin Cheuk <ycheuk03@gmail.com>
2024-04-17 21:41:58 -0700

    started char info...

---
+++ character_info.js
@@ -0,0 +1,38 @@
+const characterInfo = {
+    template: {
+        name: "aaaaa",
+        weapon: "aaaaa",
+        element: "aaaaa",
+        region: "aaaaa",
+        version: "aaaaa",
+        talents: "aaaaa",
+        stat: "aaaaa",
+        boss: "aaaaa",
+        affiliation: "aaaaa"
+    },
+    albedo: {
+        name: "Albedo",
+        weapon: "Sword",
+        element: "Geo",
+        region: "Mondstadt",
+        version: "1.2",
+        talents: "Ballad",
+        stat: "Geo DMG Bonus",
+        boss: "Tusk of Monoceros Caeli",
+        affiliation: "Knights of Favonius"
+    },
+    alhaitham: {
+        name: "Alhaitham",
+        weapon: "Sword",
+        element: "Dendro",
+        region: "Sumeru",
+        version: "3.4",
+        talents: "Ingenuity",
+        stat: "Dendro DMG Bonus",
+        boss: "Mirror of Mushin",
+        affiliation: "Sumeru Akademiya"
+    },
+};
+
+// You can also export the characterInfo object to use it in other JavaScript files
+export default characterInfo;
